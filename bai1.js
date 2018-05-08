b88b42b61c15f5c0edb68f2223e507afec44ca38
= <tqhoangcnttb94@gmail.com>
2018-05-08 18:43:19 +0700

    bai1

---
+++ bai1.js
@@ -0,0 +1,7 @@
+console.log("A = 5");
+console.log("B = 3");
+console.log("A + B =8");
+console.log("A - B =2");
+console.log("A x B =15");
+console.log("A / B =1.666666667");
+console.log("A % B = 2");
