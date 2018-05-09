e1d782f2691f067ee8610750264cb20b23b58e4b
= <tqhoangcnttb94@gmail.com>
2018-05-09 10:33:02 +0700

    bai6

---
+++ bai6.js
@@ -0,0 +1,77 @@
+var arr = new Array(' ',' ',' ',' ',' ');
+for (var  i =0 ; i<=8 ; i++ )
+{
+    if(i==0)
+    {
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==1)
+    {
+        arr[3] ='*';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==2)
+    {
+        arr[2] ='*';
+        arr[3] ='*';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==3)
+    {
+        arr[1] ='*';
+        arr[2] ='*';
+        arr[3] ='*';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==4)
+    {
+        arr[0] ='*';
+        arr[1] ='*';
+        arr[2] ='*';
+        arr[3] ='*';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==5)
+    {
+        arr[0]=' ';
+        arr[1] ='*';
+        arr[2] ='*';
+        arr[3] ='*';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==6)
+    {
+        arr[0]=' ';
+        arr[1] =' ';
+        arr[2] ='*';
+        arr[3] ='*';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==7)
+    {
+        arr[0]=' ';
+        arr[1] =' ';
+        arr[2] =' ';
+        arr[3] ='*';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+    if( i==8)
+    {
+        arr[0]=' ';
+        arr[1] =' ';
+        arr[2] =' ';
+        arr[3] =' ';
+        arr[4] ='*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
+    }
+
+
+}
