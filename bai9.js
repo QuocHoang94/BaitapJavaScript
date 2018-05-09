2f64e13ce239a5e0c2df15b82ef942edb69da217
= <tqhoangcnttb94@gmail.com>
2018-05-09 11:08:14 +0700

    bai9

---
+++ bai9.js
@@ -0,0 +1,113 @@
+var arr = new Array(' ',' ',' ',' ',' ',' ',' ',' ',' ');
+for (var i =0 ;i<=9;i++)
+{
+    if(i == 0)
+    {
+        arr[4] = '*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 1)
+    {
+        arr[0] = ' ';
+        arr[1] = ' ';
+        arr[2] = ' ';
+        arr[3] = '*';
+        arr[4] = '*';
+        arr[5] = '*';
+        arr[6] = ' ';
+        arr[7] = ' ';
+        arr[8] = ' ';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 2)
+    {
+        arr[0] = ' ';
+        arr[1] = ' ';
+        arr[2] = '*';
+        arr[3] = ' ';
+        arr[4] = '*';
+        arr[5] = ' ';
+        arr[6] = '*';
+        arr[7] = ' ';
+        arr[8] = ' ';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 3)
+    {
+        arr[0] = ' ';
+        arr[1] = '*';
+        arr[2] = ' ';
+        arr[3] = ' ';
+        arr[4] = '*';
+        arr[5] = ' ';
+        arr[6] = ' ';
+        arr[7] = '*';
+        arr[8] = ' ';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 4)
+    {
+        arr[0] = '*';
+        arr[1] = '*';
+        arr[2] = '*';
+        arr[3] = '*';
+        arr[4] = '*';
+        arr[5] = '*';
+        arr[6] = '*';
+        arr[7] = '*';
+        arr[8] = '*';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 5)
+    {
+        arr[0] = ' ';
+        arr[1] = '*';
+        arr[2] = ' ';
+        arr[3] = ' ';
+        arr[4] = '*';
+        arr[5] = ' ';
+        arr[6] = ' ';
+        arr[7] = '*';
+        arr[8] = ' ';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 6)
+    {
+        arr[0] = ' ';
+        arr[1] = ' ';
+        arr[2] = '*';
+        arr[3] = ' ';
+        arr[4] = '*';
+        arr[5] = ' ';
+        arr[6] = '*';
+        arr[7] = ' ';
+        arr[8] = ' ';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 7)
+    {
+        arr[0] = ' ';
+        arr[1] = ' ';
+        arr[2] = ' ';
+        arr[3] = '*';
+        arr[4] = '*';
+        arr[5] = '*';
+        arr[6] = ' ';
+        arr[7] = ' ';
+        arr[8] = ' ';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+    if(i == 8)
+    {
+        arr[0] = ' ';
+        arr[1] = ' ';
+        arr[2] = ' ';
+        arr[3] = ' ';
+        arr[4] = '*';
+        arr[5] = ' ';
+        arr[6] = ' ';
+        arr[7] = ' ';
+        arr[8] = ' ';
+        console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]);
+    }
+}
